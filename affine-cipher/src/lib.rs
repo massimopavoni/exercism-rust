bcba5e67133d891229838ecf6cd99f7f65bd26a4
Massimo Pavoni <maspavoni@gmail.com>
2025-04-23 23:26:05 +0200

    Fix last exercise not actually being completed

--- affine-cipher/src/lib.rs
+++ affine-cipher/src/lib.rs
@@ -1,18 +1,57 @@
-/// While the problem description indicates a return status of 1 should be returned on errors,
-/// it is much more common to return a `Result`, so we provide an error type for the result here.
+use std::iter::once;
+
+use modinverse::modinverse;
+use num_integer::gcd;
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum AffineCipherError {
     NotCoprime(i32),
 }
 
-/// Encodes the plaintext using the affine cipher with key (`a`, `b`). Note that, rather than
-/// returning a return code, the more common convention in Rust is to return a `Result`.
 pub fn encode(plaintext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
-    todo!("Encode {plaintext} with the key ({a}, {b})");
+    if gcd(a, 26) == 1 {
+        Ok(plaintext
+            .to_lowercase()
+            .bytes()
+            .filter(u8::is_ascii_alphanumeric)
+            .enumerate()
+            .filter_map(|(i, x)| {
+                let space = if i != 0 && i % 5 == 0 {
+                    Some(' ')
+                } else {
+                    None
+                }
+                .into_iter();
+
+                match x {
+                    (b'a'..=b'z') => {
+                        Some(space.chain(once(((a * (x - 97) as i32 + b) % 26 + 97) as u8 as char)))
+                    }
+                    (b'0'..=b'9') => Some(space.chain(once(x as char))),
+                    _ => None,
+                }
+            })
+            .flatten()
+            .collect())
+    } else {
+        Err(AffineCipherError::NotCoprime(a))
+    }
 }
 
-/// Decodes the ciphertext using the affine cipher with key (`a`, `b`). Note that, rather than
-/// returning a return code, the more common convention in Rust is to return a `Result`.
 pub fn decode(ciphertext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
-    todo!("Decode {ciphertext} with the key ({a}, {b})");
+    if let Some(a_modinv) = modinverse(a, 26) {
+        println!("a_modinv: {}", a_modinv);
+        Ok(ciphertext
+            .bytes()
+            .filter_map(|y| match y {
+                (b'a'..=b'z') => {
+                    Some(((a_modinv * (y as i32 - 97 - b)).rem_euclid(26) + 97) as u8 as char)
+                }
+                (b'0'..=b'9') => Some(y as char),
+                _ => None,
+            })
+            .collect())
+    } else {
+        Err(AffineCipherError::NotCoprime(a))
+    }
 }
